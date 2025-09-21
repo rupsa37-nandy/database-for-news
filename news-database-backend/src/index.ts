69f7ad3080915e869413dcf6354e008f25f136d9
rupsa37-nandy <rupsa.nandy@somnetics.in>
2025-09-21 21:27:46 +0530

    MongoDB connection is done.

--- news-database-backend/src/index.ts
+++ news-database-backend/src/index.ts
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import apps from "./app";
+import { connectDB } from "./config/dataSource";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-apps.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const connection = async () => {
+  try {
+    // connect to MongoDB
+    await connectDB();
+    // start server only if DB connection succeeds
+    apps.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to database:", error);
+  }
+};
+
+connection();
