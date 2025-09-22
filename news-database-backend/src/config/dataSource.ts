0488860fa78de72039629072e447b0330d456fc9
rupsa37-nandy <rupsa.nandy@somnetics.in>
2025-09-22 09:49:04 +0530

    Comment lines added in config/dataSource.ts

--- news-database-backend/src/config/dataSource.ts
+++ news-database-backend/src/config/dataSource.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
+//async is required because mongoose.connect() is asynchronous and returns a Promise.
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string, {});
+    //await makes your code wait until the connection is either successful or fails
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log("--MongoDB is connected--");
   } catch (error) {
-    console.error("MongoDB connection failed -> ", error);
-    process.exit(1);
+    console.log("MongoDB connection failed -> ", error);
   }
 };
