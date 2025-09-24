bd04e40fdbceaab776c476249f69dc3a18fed114
rupsa37-nandy <rupsa.nandy@somnetics.in>
2025-09-24 11:41:56 +0530

    translationmodel is added

---
+++ news-database-backend/src/models/translationmodel.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const TranslationSchema = new mongoose.Schema(
+    {
+    tid: { type: Number, required: true, unique:true},
+    src_lang: { type: String, required: true},
+    tgt_lang: { type: String, required: true},
+    text: { type: [String], required: true},
+    translations: { type: String, required: true},
+    },
+    { timestamps: true }
+);
+
+export default TranslationSchema;
