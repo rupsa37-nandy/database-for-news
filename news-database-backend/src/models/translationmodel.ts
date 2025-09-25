09dd59e2a3d194e510e9b40d5e1a358c1b42139f
rupsa37-nandy <rupsa.nandy@somnetics.in>
2025-09-25 12:42:09 +0530

    just created route,controller & service for translation.

--- news-database-backend/src/models/translationmodel.ts
+++ news-database-backend/src/models/translationmodel.ts
@@ -5,7 +5,7 @@ const TranslationSchema = new mongoose.Schema(
     tid: { type: Number, required: true, unique:true},
     src_lang: { type: String, required: true},
     tgt_lang: { type: String, required: true},
-    text: { type: [String], required: true},
+    text: { type: String, required: true},
     translations: { type: String, required: true},
     },
     { timestamps: true }
