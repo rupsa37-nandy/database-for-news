6fe721bb39a062ed1b8f26d8776990765cee3b6a
rupsa37-nandy <rupsa.nandy@somnetics.in>
2025-10-06 10:29:25 +0530

    Added translation service.

---
+++ news-database-backend/src/service/translationService.ts
@@ -0,0 +1,10 @@
+import TranslationSchema from "../models/translationmodel";
+
+// Save translated news
+export const save = async (data: any) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
