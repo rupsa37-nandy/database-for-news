ca7945d584284877d2296274bc6d908d85884712
souravghosh0 <sourav.ghosh@somnetics.in>
2025-10-06 11:46:52 +0530

    Deleted unused code

--- news-database-backend/src/routes/translationroute.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { translationNews } from "../controller/translationController";
-
-const translationRouter = Router();
-
-// Route to save the translated news
-translationRouter.post("/translate_news", translationNews);
-
-export default translationRouter;
